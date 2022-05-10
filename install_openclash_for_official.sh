6d048da0e2e93ec5cc4605112baa452e14c51aa9
igithublab <66892031+igithublab@users.noreply.github.com>
2022-05-10 22:02:05 +0800

    Update install_openclash_for_official.sh

--- install_openclash_for_official.sh
+++ install_openclash_for_official.sh
@@ -9,11 +9,11 @@ opkg install /tmp/libcap-bin_*.ipk
 
 # install luci-app-openclash, will try to get latest version later
 #https://github.com/vernesong/OpenClash/releases
-wget --no-check-certificate https://github.com/vernesong/OpenClash/releases/download/v0.42.05-beta/luci-app-openclash_0.42.05-beta_all.ipk
+wget --no-check-certificate https://github.com/vernesong/OpenClash/releases/download/v0.45.16-beta/luci-app-openclash_0.45.16-beta_all.ipk
 opkg install /tmp/luci-app-openclash_*.ipk
 
 # install openclash core, this script will download latest version openclash core.
 mkdir -p /etc/openclash/core/
 cd /etc/openclash/core/
-clash_main_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/Clash | grep /clash-linux-mipsle-hardfloat | sed 's/.*url\": \"//g' | sed 's/\"//g')
+clash_main_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/Clash | grep /clash-linux-mipsle-softfloat | sed 's/.*url\": \"//g' | sed 's/\"//g')
 wget $clash_main_url && tar zxvf clash-linux-*.tar.gz && rm -f clash-linux-*.gz
