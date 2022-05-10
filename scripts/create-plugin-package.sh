911a15def0ca5dfc1743d2b4c5da042cf4bb2294
igithublab <66892031+igithublab@users.noreply.github.com>
2022-05-10 22:06:45 +0800

    Update create-plugin-package.sh

--- scripts/create-plugin-package.sh
+++ scripts/create-plugin-package.sh
@@ -64,13 +64,25 @@ chmod +x ./plugin/install-ssrp.sh
 
 echo "Creating installation script for openclash"
 cat << EOF > ./plugin/install-openclash.sh
+
 opkg update
-opkg install luci luci-i18n-base-zh-cn ttyd luci-app-ttyd luci-i18n-ttyd-zh-cn luci-compat luci-lib-ipkg wget htop
-opkg install ./luci-app-openclash_*.ipk
-mkdir -p /etc/openclash/core
-cd /etc/openclash/core
-ARCH=mipsle-softfloat
-clash_main_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/Clash | grep /clash-linux-$ARCH | sed 's/.*url\": \"//g' | sed 's/\"//g')
+opkg install luci luci-i18n-base-zh-cn luci-compat luci-lib-ipkg wget htop libcap ttyd luci-app-ttyd luci-i18n-ttyd-zh-cn
+
+# install libcap-bin, will try to get latest version later
+#https://downloads.openwrt.org/snapshots/packages/mipsel_24kc/base/
+cd /tmp
+wget --no-check-certificate https://downloads.openwrt.org/snapshots/packages/mipsel_24kc/base/libcap-bin_2.48-1_mipsel_24kc.ipk
+opkg install /tmp/libcap-bin_*.ipk
+
+# install luci-app-openclash, will try to get latest version later
+#https://github.com/vernesong/OpenClash/releases
+wget --no-check-certificate https://github.com/vernesong/OpenClash/releases/download/v0.45.16-beta/luci-app-openclash_0.45.16-beta_all.ipk
+opkg install /tmp/luci-app-openclash_*.ipk
+
+# install openclash core, this script will download latest version openclash core.
+mkdir -p /etc/openclash/core/
+cd /etc/openclash/core/
+clash_main_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/Clash | grep /clash-linux-mipsle-softfloat | sed 's/.*url\": \"//g' | sed 's/\"//g')
 wget $clash_main_url && tar zxvf clash-linux-*.gz && rm -f clash-linux-*.gz
 chmod +x clash*
 EOF
